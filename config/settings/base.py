67cf649c4a6d901d2c75adcbca201224b3fc84ca
Oladotun Kolapo <oladotunkolapo@gmail.com>
2025-12-13 03:30:28 +0100

    settings update

--- config/settings/base.py
+++ config/settings/base.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -113,6 +114,11 @@ CORS_ALLOWED_ORIGINS = env.list(
         "DJANGO_CORS_ALLOWED_ORIGINS",
         default=[],
     )
+
+CSRF_TRUSTED_ORIGINS = env.list(
+    "CSRF_TRUSTED_ORIGINS", 
+    default=[]
+)
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
